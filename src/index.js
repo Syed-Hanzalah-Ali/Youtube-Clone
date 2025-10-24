ef335c6e07e66bcb527038c867662efd1015dada
Syed-Hanzalah-Ali <syedhanzalahali1062@gmail.com>
2025-10-24 16:34:26 +0500

    util code setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
+import dotenv from "dotenv";
+import { app } from "./app.js";
 import { connectDB } from "./db/connect.DB.js";
 
+dotenv.config({
+    path:"./env"
+})
 
 
 
 
 
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("error: ",error);
+        throw error;
+    });
 
-connectDB();
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log(`app is listening at port: ${process.env.port}`);
+        
+    })
+})
+
+.catch((error)=>{
+    console.log("MONGODB connection failed: ",error);
+});
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,42 @@
+
+export const asyncHandler=(func)=>{
+    return (req,res,next)=>{
+        Promise.resolve(func(req,res,next))
+        .catch((error)=>{
+            res.status(error.code||500).json({
+                success:false,
+                message:error.message
+            })
+        })
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const asyncHandler=(func)=>{
+//     return async (req,res,next)=>{
+//         try {
+//             await func(req,res,next);
+            
+//         } 
+//         catch (error) {
+//             res.status(error.code||500).json({
+//                 success:false,
+//                 message:error.message
+//             })    
+//         }
+
+//     }
+// }
