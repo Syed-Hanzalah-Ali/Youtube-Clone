0f27fd4a05b5839b8a97bef76746c7868c509b28
Syed-Hanzalah-Ali <syedhanzalahali1062@gmail.com>
2025-11-06 15:59:20 +0500

    all setup configured

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+const app=express();
+
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credentials:true
@@ -13,4 +15,12 @@ app.use(express.static("public"));
 app.use(cookieParser())
 
 
-export const app=express();
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration  -- we have seperated the route so we have to add middleware to use route
+app.use("/api/v1/users",userRouter);  // whatever the value in "" is used as prefix and will redirect to userRouter
+// http://localhost:3000/api/v1/users
+
+
+export {app};
